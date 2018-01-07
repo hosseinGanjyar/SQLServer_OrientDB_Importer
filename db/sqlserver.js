2b510f4dad3a961eb05cf8bb7c00fde03b7d5837
Hossein <hossein.ganjyar@gmail.com>
2018-01-08 02:08:23 +0330

    #

--- db/sqlserver.js
+++ db/sqlserver.js
@@ -1,20 +1,74 @@
 // sqlserver config
 
+const sql = require('mssql')
 
-const sqlConfig = { name: "Ken Bruen", genre: "Noir", nationality: "Irish" };
+const sqlConfig = {
+    user: 'sa',
+    password: '123s123S',
+    server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+    database: 'EmailAnalys- 2',
+};
 
-function transport(callback) {  
+function transport(callback) {
     console.log('start transport data forom sql...');
-    
+    // connect to sql streaming
+    streamRead();
 }
 
-// connectto sql
-// get records by record stream
-// push to array
+// get record by record using stream
+function streamRead() {
+    let recordsList = [];
+    sql.connect(config, err => {
+        // ... error checks
+        if (err) {
+            showMessage('error in connect to sqlserver: ' + err)
+        }
+
+        const request = new sql.Request()
+        request.stream = true // You can set streaming differently for each request
+        request.query('select Body from EmailsInfo where body is not null') // or request.execute(procedure)
+
+        request.on('recordset', columns => {
+            // Emitted once for each recordset in a query
+        })
+
+        request.on('row', row => {
+            // Emitted for each row in a recordset
+            // push to array
+            recordsList.push(row)
+        })
+
+        request.on('error', err => {
+            // May be emitted multiple times
+        })
+
+        request.on('done', result => {
+            // Always emitted as the last one
+            showMessage('all records is ready to insert!')
 // send array to orient for insert
+
+        })
+    })
+
+    sql.on('error', err => {
+        // ... error handler
+        if (err) {
+            showMessage('error in connect to sqlserver: ' + err)
+        }
+    })
+}
+
+
+
+
+
 // callback from insert
 // if success > update records in sql
 // if unSuccess > show error / repeat
 // if no more records for transport, so end of program and show message
+
+function showMessage(msg) {
+    console.log(msg)
+}
 
 module.exports.transport = transport;  
